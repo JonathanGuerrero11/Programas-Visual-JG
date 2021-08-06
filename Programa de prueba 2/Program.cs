a1021bdfa5d3a7b67fd4bc41a0381716f1fe6183
Jonathan David Guerrero Gomez <jdavidguerrero@ucompensar.edu.co>
2021-08-06 13:10:42 -0500

    Programa de Prueba 2

---
+++ Programa de prueba 2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Programa_de_prueba_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("");
+            Console.WriteLine("      COMO ARRANCAR UN AUTO MANUAL");
+            Console.WriteLine("------------------------------------------");
+            Console.WriteLine("1.  Encender el carro");
+            Console.WriteLine("2.  Incertar la llave en el arranque");
+            Console.WriteLine("3.  Asegúrate de que el carro esté en neutro");
+            Console.WriteLine("4.  Arranca el motor");
+            Console.WriteLine("5.  Presiona el embrague hasta el fondo");
+            Console.WriteLine("6.  Mueve la palanca hacia la primera marcha");
+            Console.WriteLine("7.  Mueve la palanca hacia la primera marcha");
+            Console.WriteLine("8.  Comienza a presionar el acelerador");
+            Console.WriteLine("9.  Ten cuidado de no ahogar el carro");
+            Console.WriteLine("10. Pasa a la segunda marcha");
+            Console.WriteLine("");
+            Console.WriteLine("             Listo :D");
+        }
+    }
+}
