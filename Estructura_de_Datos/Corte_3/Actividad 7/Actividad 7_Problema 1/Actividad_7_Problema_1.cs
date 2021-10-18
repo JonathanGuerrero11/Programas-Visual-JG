18674ecfe08520b7c071e1b677f3e49bedda327b
Jonathan David Guerrero Gomez <jdavidguerrero@ucompensar.edu.co>
2021-10-18 14:32:32 -0500

    Estructura de datos - Jonathan Guerrero

---
+++ Estructura_de_Datos/Corte_3/Actividad 7/Actividad 7_Problema 1/Actividad_7_Problema_1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+
+namespace Actividad_7_Problema_1
+{
+    class Actividad_7_Problema_1
+    {
+        Queue cola = new Queue();
+        int valor;
+        public void ejercicio()
+        {
+            do
+            {
+
+                Console.WriteLine($"Ingrese el valor a la cola: ");
+                valor = int.Parse(Console.ReadLine());
+                Console.WriteLine();
+                cola.Enqueue(valor);
+
+            } while (valor != 0);
+            Console.WriteLine("La lista de numeros enteros ingresados a la cola son los siguientes: ");
+            Console.WriteLine("─────────────────────");
+            foreach (int a in cola)
+            {
+                Console.Write(a + ". ");
+            }
+            Console.WriteLine("\n");
+            Console.WriteLine("─────────────────────");
+
+
+        }
+        static void Main(string[] args)
+        {
+            //Grupo: Jonathan David Guerrero, Juan Diego Rodriguez Riaño y Juan Esteban Jimenez 
+            //Creado por: Juan Diego Rodriguez Riaño
+            //Turno: Diurno
+            //Codigo: 239-2A DIA
+
+            /* Problema 1:  Escriba un programa que inserte valores ordenadamente en una lista. La lista/pila/cola
+            debe almacenar números enteros. El programa debe leer una secuencia de números
+            enteros de la entrada hasta que se ingrese el número cero*/
+            Actividad_7_Problema_1 llamado = new Actividad_7_Problema_1();
+            Console.WriteLine("--Ejercicio 1---");
+            Console.WriteLine("Puede ingresar una cantidad ilimitada de numeros a la cola, para terminarla escriba cero");
+            Console.WriteLine();
+            Console.WriteLine("─────────────────────");
+            llamado.ejercicio();
+            Console.WriteLine();
+            Console.WriteLine("Fin del programa, de enter si quiere limpiar la pantalla");
+            Console.ReadKey();
+            Console.Clear();
+
+
+        }
+    }
+}
---
+++ Estructura_de_Datos/Corte_3/Actividad 7/Actividad 7_Problema 2/Actividad_7_Problema_2.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections;
+
+namespace Actividad_7_Problema_2
+{
+    class Actividad_7_Problema_2
+    {
+        Queue cola = new Queue();
+        int ca, num;
+        public void cuadrados()
+        {
+
+            Console.WriteLine("Ingrese la cantidad de numeros que quiere ingresar en la cola");
+            ca = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+            for (int a = 0; a < ca; a++)
+            {
+                Console.WriteLine();
+                Console.WriteLine($"Ingrese valor {a + 1}");
+                num = int.Parse(Console.ReadLine());
+                cola.Enqueue(num);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("La sumatoria de los elementos dentro de la lista son los siguientes: ");
+            Console.WriteLine("─────────────────────");
+            foreach (int a in cola)
+            {
+                int b = a + 1;
+                int c = a + b;
+                int total = a * b * c;
+                int sumatoria = total / 6;
+
+                Console.WriteLine("-" + sumatoria);
+
+            }
+
+
+
+        }
+
+
+        static void Main(string[] args)
+        {
+            //Grupo: Jonathan David Guerrero, Juan Diego Rodriguez Riaño y Juan Esteban Jimenez 
+            //Creado por: Juan Diego Rodriguez Riaño
+            //Turno: Diurno
+            //Codigo: 239-2A DIA
+
+            /* Problema 2: Escriba un programa que calcule la sumatoria de los cuadrados de los elementos de una
+            lista/pila/cola de números enteros.
+            */
+            Actividad_7_Problema_2 llamado = new Actividad_7_Problema_2();
+
+
+            Console.WriteLine("--EJERCICIO 2--");
+            Console.WriteLine("La sumatoria cuadrada de N numeros ingresados en una cola");
+            Console.WriteLine("─────────────────────");
+            llamado.cuadrados();
+            Console.WriteLine("─────────────────────");
+            Console.WriteLine("Fin del programa, de enter para limpiar pantalla");
+            Console.ReadKey();
+            Console.Clear();
+
+
+
+
+        }
+    }
+}
